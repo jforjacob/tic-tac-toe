752bdb5a8e9a63db26e7e8ef1e429d9fd8f515b2
Jacob Shemkovitz <jacob.shemkovitz@gmail.com>
2015-08-26 23:46:35 -0400

    two player game functionality almost complete

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,18 +1,33 @@
 $(function(){
 
-var gamegrid = [ null, null, null,
-				 null, null, null,
-				 null, null, null ];
+var gamegrid = [ null, null, null, null, null, null, null, null, null ];
 
-var winningLines = [ [0,1,2], [0,4,8], [0,3,6], [1,4,7],
-					 [2,4,6], [2,5,8], [3,4,5], [6,7,8] ];
+var winningLines = [ [0,1,2], [0,4,8],
+										 [0,3,6], [1,4,7],
+										 [2,4,6], [2,5,8],
+										 [3,4,5], [6,7,8] ];
 
-var players = [ 'O', 'X' ];
+var players = [ { 'mark': 'O',
+									'name': 'O',
+									'score': 0 },
+								{ 'mark': 'X',
+									'name': 'X',
+									'score': 0 } ];
 
-var currentPlayer = 1;
+var currentPlayer = 0;
+var roundNum = 0;
 var turns = 0;
+var roundOver = false;
 
-$('.turn').text("It is "+players[ currentPlayer ]+"'s turn.");
+// $.Velocity
+//     .RegisterEffect("transition.flipXIn", {
+//         defaultDuration: 700,
+//         calls: [
+//             [ { opacity: 1, rotateY: [ 0, -55 ] } ]
+//         ]
+//     });
+
+$('.turn').text("It is "+players[ currentPlayer ].name+"'s turn.");
 
 
 // Make the game board:
@@ -20,58 +35,86 @@ $('.turn').text("It is "+players[ currentPlayer ]+"'s turn.");
 createBoard();
 
 function createBoard() {
-	var n = 0;
+	gamegrid = [ null, null, null, null, null, null, null, null, null ];
+	$('#gameboard').empty();
+	switchPlayers();
+	var sq = 0;
+	roundNum++;
+	$('.message').text("Round "+roundNum+" || Player "+players[0].name+": "+players[0].score+" || Player "+players[1].name+": "+players[1].score);
 
 	for( var i=0; i<3; i++ ) {
 		var row = $('<div>').addClass('row clearfix');
 		row.appendTo( $('#gameboard') );
 
 		for( var j=0; j<3; j++ ) {
-			var square = $('<div>').addClass('square');
-			var idName = 'sq'+n;
+			var square = $('<div>')
+			square.addClass('square');
+			var idName = 'sq'+sq;
 			square.attr('id', idName );
-			n++;
+			sq++;
 			square.appendTo( row );
+			// square.on('load', function() {
+			// 	square.velocity("transition.flipXin");
+			// });
 		}
 	}
+	$('.square').on('click', takeTurn );
 }
-
-$('.square').on('click', takeTurn );
 
 function takeTurn(e) {
-	if( $(e.currentTarget).hasClass('X') || $(e.currentTarget).hasClass('O') ) {
+	if( $(e.target).hasClass('X') || $(e.target).hasClass('O') ) {
 		$('.message').text("That space is already taken!");
-		setTimeout( function(){
+		$("div").velocity("callout.shake");
+		setTimeout( function() {
 			$('.message').text("");
-		}, 2000);
+		}, 2000 );
+		
 	} else {
-		turns++;
-		changeTheGrid(e);
-		checkForWin();
-		switchPlayers();
+		changeArray(e);
+		roundOver = checkForEnd();
+		console.log(roundOver);
+		if(roundOver) { 
+			changeTheBoard(e);
+			setTimeout( function(){
+				roundOver = false;
+				createBoard();
+			}, 2000);
+		} else {
+			changeTheBoard(e);
+			switchPlayers();
+		}
+		
 	}
 }
 
-function changeTheGrid( e ) {
-	var playerClass = players[ currentPlayer ];
-	$(e.currentTarget).addClass( playerClass );
-	var position = $(e.currentTarget).attr('id').charAt(2);
+
+
+function changeArray(e) {
+	var position = $(e.target).attr('id').charAt(2);
 	gamegrid.splice( position, 1, currentPlayer );
-	$(e.currentTarget).text( players[ currentPlayer ] );
-	console.log( gamegrid );
 }
 
-function checkForWin(){
+function checkForEnd(){
+	var end = false;
 	winningLines.forEach( function( line ) {
-		// if all criteria match one of the winning circumstances
-		if( gamegrid[ line[0] ] == gamegrid[ line[1] ] && gamegrid[ line[1] ] == gamegrid[ line[2] ] ) {
-			if( gamegrid[ line[0] ] === 1 || gamegrid[ line[0] ] === 0 ) {
-				$('.message').text( players[ gamegrid[ line[0] ] ] + " wins!");
-			}
-		} else if( turns === 9 ) {
+		// if all criteria match one of the winning circumstances in array winningLines
+		if( gamegrid[ line[0] ] === gamegrid[ line[1] ] && gamegrid[ line[1] ] === gamegrid[ line[2] ] &&
+			( gamegrid[ line[0] ] === 1 || gamegrid[ line[0] ] === 0 ) ) {
+			$('.message').text( players[ gamegrid[ line[0] ] ].name + " wins!");
+			players[ gamegrid[ line[0] ] ].score++; // this can trigger multiple times - fix!
+			end = true;
+		} else if ( gamegrid.indexOf( null ) === -1 ) {
 			$('.message').text( "Draw!" );
+			end = true;
 		}
 	});
+	return end;
+}
+
+function changeTheBoard(e) {
+	var playerMark = players[ currentPlayer ].mark;
+	$(e.target).addClass( playerMark );
+	$(e.target).text( playerMark );
 }
 
 function switchPlayers() {
@@ -80,7 +123,7 @@ function switchPlayers() {
 	} else {
 		currentPlayer = 0;
 	}
-	$('.turn').text("It is "+players[ currentPlayer ]+"'s turn.");
+	$('.turn').text("It is "+players[ currentPlayer ].name+"'s turn.");
 }
 
 });
